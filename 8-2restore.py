587abff885689f461b23f702afd03ac58a2b71de
yszpatt <yszpat@qq.com>
2018-10-15 14:26:33 +0800

    First Push

---
+++ 8-2restore.py
@@ -0,0 +1,52 @@
+import tensorflow as tf
+import input_data
+
+mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+# 数据批次
+batch_size = 100
+n_batch = mnist.train.num_examples // batch_size
+
+x = tf.placeholder(tf.float32, [None, 784])
+y = tf.placeholder(tf.float32, [None, 10])
+
+# l1 = tf.layers.dense(x, 10, tf.nn.tanh)
+# l1 = tf.layers.dense(x, 20, tf.nn.relu)
+output = tf.layers.dense(x, 10, tf.nn.softmax)
+
+# Weights_L1 = tf.Variable(tf.zeros([784, 10]))
+# biases_L1 = tf.Variable(tf.zeros([10]))
+# output = tf.nn.softmax(tf.matmul(x, Weights_L1) + biases_L1)
+
+# 线性激励函数使用二次代价函数
+# loss = tf.losses.mean_squared_error(y, output)
+
+# S型激励函数使用交叉熵求平均
+# loss = tf.reduce_mean(
+#     tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=output))
+loss = tf.losses.softmax_cross_entropy(y, output)
+
+# （对数释然函数）
+# loss = tf.reduce_mean(-tf.reduce_sum(y *
+#                                      tf.log(output), reduction_indices=[1]))
+
+
+# 梯度下降
+train_op = tf.train.GradientDescentOptimizer(0.3).minimize(loss)
+# train_op = tf.train.AdamOptimizer(1e-2).minimize(loss)
+
+
+init = tf.global_variables_initializer()
+
+# 用于检测测试数据是否正确的bool
+correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(output, 1))
+# bool列表转换为float32格式
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+saver = tf.train.Saver()
+with tf.Session() as sess:
+    sess.run(init)
+    print(sess.run(accuracy, feed_dict={
+          x: mnist.test.images, y: mnist.test.labels}))
+    saver.restore(sess, 'net/mynet.ckpt')
+    print(sess.run(accuracy, feed_dict={
+          x: mnist.test.images, y: mnist.test.labels}))
